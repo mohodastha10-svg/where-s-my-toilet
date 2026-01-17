b759c3a9b254516815b911355435c7a5d46ba2fa
mohodastha10-svg <mohodastha10@gmail.com>
2026-01-18 05:14:03 +0530

    integrated backend

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const demandController = require('../controllers/demandController');
-
-// This defines the /api/search path
-router.post('/search', demandController.handleSearch);
-
-module.exports = router;
